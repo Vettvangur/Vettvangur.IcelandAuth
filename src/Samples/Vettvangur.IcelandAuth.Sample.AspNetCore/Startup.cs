892c2a36e72abbc825ae637bd52a990f43146ed8
Gunnar Már Óttarsson <gunni@starcraft.is>
2020-09-12 11:04:14 +0000

    AspNetCore Sample minor

--- src/Samples/Vettvangur.IcelandAuth.Sample.AspNetCore/Startup.cs
+++ src/Samples/Vettvangur.IcelandAuth.Sample.AspNetCore/Startup.cs
@@ -23,7 +23,7 @@ namespace Vettvangur.IcelandAuth.Sample.AspNetCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,11 +35,11 @@ namespace Vettvangur.IcelandAuth.Sample.AspNetCore
             }
             else
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseExceptionHandler("/Home/Error");
             }
 
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
