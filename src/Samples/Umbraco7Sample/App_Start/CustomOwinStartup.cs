cb6ca1b551db87aa360ad4a43893f7928dce4abd
Gunnar Már Óttarsson <gunni@starcraft.is>
2020-11-06 13:50:39 +0000

    update 7 sample with owin backoffice integration

---
+++ src/Samples/Umbraco7Sample/App_Start/CustomOwinStartup.cs
@@ -0,0 +1,24 @@
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using Umbraco.Web;
+using Umbraco7Sample.App_Start;
+
+[assembly: OwinStartup("CustomOwinStartup", typeof(CustomOwinStartup))]
+
+namespace Umbraco7Sample.App_Start
+{
+    public class CustomOwinStartup : UmbracoDefaultOwinStartup
+    {
+        protected override void ConfigureUmbracoAuthentication(IAppBuilder app)
+        {
+            base.ConfigureUmbracoAuthentication(app);
+            app.ConfigureBackOfficeIcelandAuth(
+                postLoginRedirectUri: ConfigurationManager.AppSettings["IcelandAuth:Owin:RedirectUrl"]);
+        }
+    }
+}
