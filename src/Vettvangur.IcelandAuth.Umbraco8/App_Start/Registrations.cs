de680fc3dae058dd31b54fb2bdc39036863cc13b
Gunnar Már Óttarsson <gunni@starcraft.is>
2021-03-11 15:46:34 +0000

    Fix U8 registrations

--- src/Vettvangur.IcelandAuth.Umbraco8/App_Start/Registrations.cs
+++ src/Vettvangur.IcelandAuth.Umbraco8/App_Start/Registrations.cs
@@ -22,7 +22,14 @@ namespace Vettvangur.IcelandAuth.Umbraco8.App_Start
                 fac.GetInstance<Umbraco.Core.Logging.ILogger>(),
                 typeof(IcelandAuthService)));
 
-            composition.Register<IcelandAuthService>();
+            // We would love to have this lifetime Transient
+            // Umbraco however doesn't provide an easy way to hook into the startup lifecycle until
+            // after the first request to the LightInject container is made
+            // https://github.com/seesharper/LightInject/issues/133
+            // After that first request, registrations to the container are not overridable
+            // unless we use the Umbraco custom RegisterUnique which keeps it's own Dictionary
+            // of registrations.
+            composition.RegisterUnique<IcelandAuthService>();
         }
     }
 }
