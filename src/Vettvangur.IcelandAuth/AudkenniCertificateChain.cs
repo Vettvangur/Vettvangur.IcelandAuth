ba1313e239981fecd20eab7b530238e160776feb
Gunnar Már Óttarsson <gunni@starcraft.is>
2021-04-16 16:15:28 +0000

    Fix non-windows platform compatibility, see https://github.com/Vettvangur/Vettvangur.IcelandAuth/issues/8

--- src/Vettvangur.IcelandAuth/AudkenniCertificateChain.cs
+++ src/Vettvangur.IcelandAuth/AudkenniCertificateChain.cs
@@ -1,13 +1,14 @@
 #if NET5
+using System;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Vettvangur.IcelandAuth
 {
     partial class AudkenniCertificateChain
     {
-        public static X509Certificate2 Audkennisrot => new X509Certificate2(AudkennisrotBytes);
-        public static X509Certificate2 TrausturBunadur => new X509Certificate2(TrausturBunadurBytes);
-        public static X509Certificate2 TraustAudkenni => new X509Certificate2(TraustAudkenniBytes);
+        public static X509Certificate2 Audkennisrot => new X509Certificate2(Convert.FromBase64String(AudkennisrotBase64));
+        public static X509Certificate2 TrausturBunadur => new X509Certificate2(Convert.FromBase64String(TrausturBunadurBase64));
+        public static X509Certificate2 TraustAudkenni => new X509Certificate2(Convert.FromBase64String(TraustAudkenniBase64));
     }
 }
 #endif
