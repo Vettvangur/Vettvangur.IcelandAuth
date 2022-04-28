5af0e6b6066f44bf87fc121c09ef736caac341c6
Gunnar Már Óttarsson <gunni@starcraft.is>
2022-04-28 18:11:52 +0000

    Umbraco 9 integration release

---
+++ src/libraries/Vettvangur.IcelandAuth.Umbraco9/IcelandAuthController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Vettvangur.IcelandAuth.UmbracoShared;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Vettvangur.IcelandAuth.Umbraco8
+{
+    /// <summary>
+    /// /umbraco/surface/icelandAuth/Login
+    /// </summary>
+    public class IcelandAuthController : SurfaceController
+    {
+        private ControllerBehavior AuthHandler;
+        public IcelandAuthController(
+            IUmbracoContextAccessor umbracoContextAccessor,
+            IUmbracoDatabaseFactory databaseFactory,
+            ServiceContext services,
+            AppCaches appCaches,
+            IProfilingLogger profilingLogger,
+            IPublishedUrlProvider publishedUrlProvider,
+            IcelandAuthService icelandAuthService,
+            IConfiguration configuration)
+            : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+            AuthHandler = new ControllerBehavior(icelandAuthService, configuration);
+        }
+
+        public ActionResult Login()
+        {
+            return Redirect(AuthHandler.Login(Request));
+        }
+    }
+}
